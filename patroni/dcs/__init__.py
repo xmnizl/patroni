e8b44f02ba7505a8cc8c0ebe6aa63d79fbb0d2b9
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2021-03-15 08:18:13 +0100

    Use dbname instead of database in connection strings
    The `database` keyword seems to be legacy and not supported by psycopg3
    anymore.

--- patroni/dcs/__init__.py
+++ patroni/dcs/__init__.py
@@ -153,7 +153,7 @@ class Member(namedtuple('Member', 'index,name,session,data')):
         defaults = {
             "host": None,
             "port": None,
-            "database": None
+            "dbname": None
         }
         ret = self.data.get('conn_kwargs')
         if ret:
@@ -167,7 +167,7 @@ class Member(namedtuple('Member', 'index,name,session,data')):
             ret = {
                 'host': r.hostname,
                 'port': r.port or 5432,
-                'database': r.path[1:]
+                'dbname': r.path[1:]
             }
             self.data['conn_kwargs'] = ret.copy()
 
